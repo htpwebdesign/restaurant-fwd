363fa28424df6655f1bb52ff168587ab6b02974c
SherYarSaeed <shersaed0107@gmail.com>
2022-06-29 15:12:39 -0700

    Adding Store Info to Footer

--- footer.php
+++ footer.php
@@ -13,17 +13,30 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'restaurant-fwd' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'restaurant-fwd' ), 'WordPress' );
+	<?php 
+		if ( function_exists ( 'get_field' ) ) {
+			if ( get_field( 'restaurant_name', 'option' ) ) {
+	?>
+		<section class= "restaurant-section">
+			<h3><?php the_field( 'restaurant_name', 'option' );?> </h3> 
+	<?php
+		}
+		if ( get_field( 'restaurant_hours', 'option' ) ) {
+			?>
+					<p><?php the_field( 'restaurant_hours', 'option' );?> </p>
+			<?php
+			}
+			if ( get_field( 'restaurant_phone_number', 'option' ) ) {
 				?>
-			</a>
-			<span class="sep"> | </span>
+						<p><?php the_field( 'restaurant_phone_number', 'option' );?> </p>
+					</section> 
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'restaurant-fwd' ), 'restaurant-fwd', '<a href="https://restaurant.bcitwebdeveloper.ca">FWD30</a>' );
-				?>
+				}
+		}
+
+	
+	
+	?>			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
