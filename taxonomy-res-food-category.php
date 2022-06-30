31f78d025553798b382851b0c7b6cf7fd2004ad3
sekhon-44 <sekhongaven@yahoo.com>
2022-06-30 15:26:55 -0700

    Update taxonomy-res-food-category.php

--- taxonomy-res-food-category.php
+++ taxonomy-res-food-category.php
@@ -22,15 +22,103 @@ get_header();
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
+
+
+				$image = get_field('image_single_menu', 'option');
+				$size = 'large'; // (thumbnail, medium, large, full or custom size)
+				if( $image ) {
+					echo wp_get_attachment_image( $image, $size );
+				}
+
+
+$terms = get_terms( 
+    array(
+        'taxonomy' => 'res-food-category',
+		'parent'   => get_queried_object()->term_id,
+		'hide_empty' => false,
+    ) 
+);
+if ( $terms && ! is_wp_error($terms) ) : ?>
+    <section>
+        <h2>Explore our menu</h2>
+			<ul>
+				<?php foreach ( $terms as $term ) : ?>
+					<li>
+						<a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+    </section>
+
+<?php endif; ?>
+
+			<?php  
 			while ( have_posts() ) :
 				the_post();
+			?>		
 
+	
+				<article <?php post_class( 'menu-card' ); ?>>
+				<h2><?php the_title(); ?></h2>
+
+				<?php 
+					if (function_exists('get_field') ) {
+						if ( get_field( 'food_description' ) ) {
+							?>
+							<section class= "food-description">
+								<p><?php the_field( 'food_description' );?> </p>
+							</section> 
+							<?php
+						}
+						if ( get_field( 'food_price' ) ) {
+							?>
+							<section class= "food-price">
+								<p> <?php the_field( 'food_price' ); ?> </p>
+							</section> 
+							<?php
+						}
+						if ( get_field( 'food_flavour' ) ) {
+							?>
+							<section class= "food-flavour">
+								<?php 
+									$flavors = get_field('food_flavour');
+									if ($flavors) :
+									?>
+									<ul>
+										<?php foreach( $flavors as $flavor) : ?>
+											<li><?php echo $flavor; ?></li>
+											<?php endforeach; ?>
+									</ul>
+									<?php endif; ?>
+							</section> 
+							<?php
+						}
+					}
+
+				?>
+			</article>
+			<?php  
 
 			endwhile;
-			?>
 
-<?php  
+			?>
+			<?php 
+				$term = get_queried_object();
+				$images = get_field('gallery', $term);
+				$size = 'full'; // (thumbnail, medium, large, full or custom size)
+				if( $images ): ?>
+					<ul>
+						<?php foreach( $images as $image_id ): ?>
+							<li>
+								<?php echo wp_get_attachment_image( $image_id, $size ); ?>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				<?php endif; ?>
+				<?php  
+
+			the_posts_navigation();
+
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
@@ -38,8 +126,12 @@ get_header();
 		endif;
 		?>
 
+
+		<div class="container">
+			<?php $terms = get_terms( 'res-food-category' ); ?>
+		</div>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
