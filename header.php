79c24c26a46a51381d7493ed0cae140f2adc2d4c
Sami Adli <samiadli00@gmail.com>
2022-07-12 12:25:05 -0700

    Added Fonts link

--- header.php
+++ header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,700;1,600&family=Raleway:wght@700&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
